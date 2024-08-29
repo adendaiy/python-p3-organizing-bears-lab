8750cc4df553b72f4925f51035460c3c916a0afc
adendaiy <adenabdib@gmail.com>
2024-08-29 23:50:48 +0100

    this is sql lab

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 10, 'M', 'brown', 'calm', 1),
+('Rowdy', 7, 'M', 'black', 'aggressive', 1),
+('Tabitha', 5, 'F', 'white', 'playful', 1),
+('Sergeant Brown', 8, 'M', 'brown', 'serious', 1),
+('Melissa', 6, 'F', 'black', 'gentle', 1),
+('Grinch', 11, 'M', 'gray', 'grumpy', 1),
+('Wendy', 4, 'F', 'white', 'curious', 1),
+(NULL, 3, 'F', 'brown', 'timid', 1);
